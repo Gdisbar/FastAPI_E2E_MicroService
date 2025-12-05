0de391d972b1a1e08396cbe66b6cf4716cfb3c13
Pritam Das <dey.abi395@gmail.com>
2025-12-05 21:03:53 +0530

    commit-2

--- src/main.py
+++ src/main.py
@@ -42,7 +42,29 @@ async def get_todos_by_id(db:db_dependency,todos_id:int=Path(gt=0)):
 
 @app.post("/todo/",status_code=status.HTTP_201_CREATED)
 async def create_todos(db:db_dependency,todo_request:TodoRequest):
-    todo_data = Todos(**todo_request.model_dump())
-    db.add(todo_data)
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
     db.commit()
-    return f"Data insterd to DB"
+
+
+
+@app.put("/todo/{todos_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_todos(db:db_dependency,todo_request:TodoRequest,todos_id:int=Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todos_id).first()
+    if todo_model is None:
+        return HTTPException(status_code=404,detail="Item not found")
+    
+    for field in todo_request.model_fields_set:
+        setattr(todo_model, field, getattr(todo_request, field))
+    
+    db.add(todo_model)
+    db.commit()
+    db.refresh(todo_model)
+
+@app.delete("/todo/{todos_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todos_by_id(db:db_dependency,todos_id:int=Path(gt=0)):
+    todos_model = db.query(Todos).filter(Todos.id == todos_id).first()
+    if todos_model is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db.query(Todos).filter(Todos.id == todos_id).delete()
+    db.commit()
